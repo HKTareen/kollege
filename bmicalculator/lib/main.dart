fccd934d5445b910051f1aa544717d7c9d10ae6f
HKTareen <hamzakhan6544@gmail.com>
2021-06-22 21:18:16 +0500

    Update main.dart

--- bmicalculator/lib/main.dart
+++ bmicalculator/lib/main.dart
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: BmiCalculate()
     );
