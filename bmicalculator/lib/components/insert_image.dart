77794622953a82ab53ae96c2269034952e9edcc5
HKTareen <hamzakhan6544@gmail.com>
2021-06-28 12:13:19 +0500

    imagePicker

--- bmicalculator/lib/components/insert_image.dart
+++ bmicalculator/lib/components/insert_image.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:myfirst/components/constants.dart';
 
 class InsertImage extends StatefulWidget {
@@ -7,16 +10,26 @@ class InsertImage extends StatefulWidget {
 }
 
 class _InsertImageState extends State<InsertImage> {
+  File _image;
+  Future getImage() async{
+    var image =  ImagePicker.platform.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image as File;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Container(
+      child:
+      Container(
         width: 170,
         height: 170,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(300),
             border: Border.all(color: Colors.black, width: 5)),
-        child: Column(mainAxisAlignment: MainAxisAlignment.end,
+        child: _image==null?
+        Column(mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Text('Please Select Image',style: TextStyle(color: KisAuctionPrimaryClr.shade100,fontSize: 15),),
             SizedBox(height: 45,),
@@ -25,8 +38,10 @@ class _InsertImageState extends State<InsertImage> {
               child: Icon(Icons.add_a_photo,size: 26,),
             )
           ],
-        ),
+        ):
+            Image.file(_image,)
       ),
+      onTap: getImage,
     );
   }
 }
